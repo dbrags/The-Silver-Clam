e45feb35d8cbda6e89f6497bba9187e2a37f44dd
dbrags <91096301+dbrags@users.noreply.github.com>
2025-10-28 10:00:57 -0400

    silver homepage

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -13,66 +13,65 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
-    >
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the index.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      <nav>
+        <div className="mx-auto px-8 py-2 flex items-center justify-between">
+          <img
+            src="/images/clam-logo.png"
+            alt="The Silver Clam Logo"
+            width="105px"
+            height="93px"
+          />
+          <div>
+            <a href="/" className="px-4">
+              Instagram
+            </a>
+            <a href="/about" className="px-4">
+              AirBNB
+            </a>
+            <a href="/gallery" className="px-4">
+              Contact
+            </a>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+      </nav>
+      <section>
+        <div className="mx-auto p-8">
+          <div className="grid grid-cols-5 gap-10">
+            <div className=" col-span-3 max-w-[820px]">
+              <h1 className="text-[122px] leading-[100%]">
+                Welcome to the Sliver Clam
+              </h1>
+              <p className="text-[16px] leading-[120%] max-w-[550px]">
+                There’s something special about waking up to the stillness of
+                the lake, coffee in hand, as the morning light dances on the
+                water. The Silver Clam is a place to slow down, breathe deeply,
+                and enjoy the simple moments— paddling on a beautiful lake,
+                gathering around the fire under a starry sky, or curling up by
+                the fireplace with a good movie after a day outdoors.
+              </p>
+            </div>
+            <img
+              className=" col-span-2"
+              src="/images/IMG_8701.jpg"
+              alt="Lake and dock view from The Silver Clam"
+              width="100%"
+              height="100%"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs/pages/getting-started?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
         </div>
-      </main>
+      </section>
+      <section>
+        <div className="mx-auto p-8">
+          <img
+            className=" col-span-2"
+            src="/images/IMG_8710.jpg"
+            alt="The Silver Clam House"
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </section>
     </div>
   );
 }
