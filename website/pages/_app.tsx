5db8347bd321f9c54cbfea538ed3b61c8b548701
dbrags <91096301+dbrags@users.noreply.github.com>
2025-10-29 13:40:03 -0400

    gallery fix

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
+import "yet-another-react-lightbox/styles.css";
 
 import "../styles/globals.css";
 
